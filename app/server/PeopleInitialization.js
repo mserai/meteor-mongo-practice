56ea32003b9f545d323b81d21e6ce4a657513bbe
mserai <mserai@hawaii.edu>
2017-10-09 19:39:27 -1000

    fin

--- app/server/PeopleInitialization.js
+++ app/server/PeopleInitialization.js
@@ -6,6 +6,5 @@ if (people.find().count() === 0) {
   people.insert({ first: "Jenna", last: "Corin", age: 31, city: "Boulder Creek" });
   people.insert({ first: "Katie", last: "Kai", age: 25, city: "Palo Alto" });
   people.insert({ first: "Doris", last: "Johnson", age: 93, city: "Naples" });
-  people.insert({ first: "Doris", last: "Johnson", age: 93, city: "Naples" });
-
 }
+//people.insert({ animal: "Dog", name: "Koa", city: "Pearl City" });
